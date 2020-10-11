58c6dcecb84b1dad9930a533b93d9064119d4e5b
Hannah Lim <hjeong.lim@gmail.com>
2020-10-10 22:52:46 -0700

    Connect about, contact pages and add compose page

--- app.js
+++ app.js
@@ -15,18 +15,25 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res) {
+    res.render("home", {StartingContent: homeStartingContent});
+});
 
+app.get("/about", function(req, res) {
+    res.render("about", {aboutContent: aboutContent});
+});
 
+app.get("/contact", function(req, res) {
+    res.render("contact", {contactContent: contactContent});
+});
 
+app.get("/compose", function(req, res) {
+    res.render("compose");
+});
 
-
-
-
-
-
-
-
-
+app.post("/compose", function(req, res) {
+    console.log(req.body.postTitle);
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
